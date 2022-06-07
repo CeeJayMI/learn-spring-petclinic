efeb9c841a5421e084fc266e89765d78fad1958a
ceejaymi <jonathan@jonathanslaven.com>
2022-06-07 12:35:49 -0400

    Initial commit

---
+++ src/main/java/com/jonathanslaven/learnspringpetclinic/LearnSpringPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.jonathanslaven.learnspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringPetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathanslaven/learnspringpetclinic/LearnSpringPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathanslaven.learnspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
