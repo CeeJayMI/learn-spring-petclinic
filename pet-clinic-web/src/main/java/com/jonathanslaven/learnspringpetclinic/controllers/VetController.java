5365b2731574e644252ceb4d767b494c074e5ceb
ceejaymi <jonathan@jonathanslaven.com>
2022-06-08 16:47:57 -0400

    refactor request mapping

--- pet-clinic-web/src/main/java/com/jonathanslaven/learnspringpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/jonathanslaven/learnspringpetclinic/controllers/VetController.java
@@ -3,10 +3,11 @@ package com.jonathanslaven.learnspringpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"", "/", "/index", "/vs/index.html"})
     public String listVets() {
         return "vets/index";
     }
